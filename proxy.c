0bff6635011b0ebcc5dbcd9d93ce1d697fbd42fc
bewisesh91 <bewise.seunghyun@gmail.com>
2021-12-23 01:48:35 +0000

    update

--- proxy.c
+++ proxy.c
@@ -5,14 +5,10 @@
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 #define CACHE_OBJS_COUNT 10
-#define LRU_MAGIC_NUMBER 9999
-// Least Recently Used
-// LRU: 가장 오랫동안 참조되지 않은 페이지를 교체하는 기법
+#define LRU_MAGIC_NUMBER 9999 // LRU, Least Recently Used : 가장 오랫동안 참조되지 않은 페이지를 교체하는 기법
 
 /* You won't lose style points for including this long line in your code */
-static const char *user_agent_hdr =
-    "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
-    "Firefox/10.0.3\r\n";
+static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 ""Firefox/10.0.3\r\n";
 static const char *requestline_hdr_format = "GET %s HTTP/1.0\r\n";
 static const char *endof_hdr = "\r\n";
 static const char *host_hdr_format = "Host: %s\r\n";
@@ -24,22 +20,23 @@ static const char *connection_key = "Connection";
 static const char *proxy_connection_key = "Proxy-Connection";
 static const char *user_agent_key = "User-Agent";
 
-void *thread(void *vargsp);
 void doit(int connfd);
 void parse_uri(char *uri, char *hostname, char *path, int *port);
 void build_http_header(char *http_header, char *hostname, char *path, int port, rio_t *client_rio);
 int connect_endServer(char *hostname, int port, char *http_header);
 
-// cache function
+// 쓰레드 생성 관련 함수
+void *thread(void *vargsp);
+
+// 캐시 관련 함수
 void cache_init();
 int cache_find(char *url);
 void cache_uri(char *uri, char *buf);
-
 void readerPre(int i);
 void readerAfter(int i);
 
-typedef struct
-{
+// 캐시 블럭 구조체
+typedef struct{
   char cache_obj[MAX_OBJECT_SIZE];
   char cache_url[MAXLINE];
   int LRU;
@@ -50,65 +47,78 @@ typedef struct
   sem_t rdcntmutex; // protects accesses to readcnt
 } cache_block;
 
-typedef struct
-{
+// 캐시 구조체
+typedef struct{
   cache_block cacheobjs[CACHE_OBJS_COUNT]; // ten cache blocks
   int cache_num;
 } Cache;
 
 Cache cache;
 
-int main(int argc, char **argv)
-{
-  int listenfd, connfd;
+
+int main(int argc, char **argv){ // argc : 인자의 개수, argv : 인자의 배열
+  int listenfd, *connfd;
   socklen_t clientlen;
   char hostname[MAXLINE], port[MAXLINE];
   pthread_t tid;
   struct sockaddr_storage clientaddr;
 
+  // cache_init : 
   cache_init();
 
-  if (argc != 2)
-  {
-    // fprintf: 출력을 파일에다 씀. strerr: 파일 포인터
+  /* Check command line args */
+  if (argc != 2){ // 입력된 인자의 갯수가 2개가 아니라면 표준 오류 출력
     fprintf(stderr, "usage: %s <port> \n", argv[0]);
     exit(1); // exit(1): 에러 시 강제 종료
   }
-  Signal(SIGPIPE, SIG_IGN);
-  listenfd = Open_listenfd(argv[1]);
-  while (1)
-  {
-    clientlen = sizeof(clientaddr);
-    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
 
+  // Signal(SIGPIPE, SIG_IGN) : 비정상 종료된 소켓에 접근 시 발생하는 프로세스 종료 시그널을 무시하는 함수
+  // 이를 통해 나머지 클라이언트들과이 연결 중인 전체 프로세스를 유지할 수 있음
+  Signal(SIGPIPE, SIG_IGN);
+  
+  // Open_listenfd 함수를 호출해서 듣기 소켓을 오픈한다. 인자로 포트 번호를 넘겨준다.
+  listenfd = Open_listenfd(argv[1]);
+  while (1){
+    clientlen = sizeof(clientaddr);
+    connfd = Malloc(sizeof(int));
+
+    // 반복적으로 연결 요청을 접수한다. accept 함수는 1. 듣기 식별자, 2. 소켓 주소 구조체의 주소, 3. 주소(소켓 구조체)의 길이를 인자로 받는다.
+    *connfd = Accept(listenfd,(SA *)&clientaddr,&clientlen);
+
+    // Getaddrinfo는 호스트 이름(호스트 주소), 서비스 이름(포트 번호)의 스트링 표시를 소켓 주소 구조체로 변환한다.
+    // Getnameinfo는 위를 반대로 소켓 주소 구조체에서 스트링 표시로 변환한다.
+    // 연결이 성공했다는 메시지를 위해 Getnameinfo 함수를 호출한다.
     Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
     printf("Accepted connection from (%s %s).\n", hostname, port);
 
-    // 첫 번째 인자 *thread: 쓰레드 식별자
-    // 두 번째: 쓰레드 특성 지정 (기본: NULL)
-    // 세 번째: 쓰레드 함수
-    // 네 번째: 쓰레드 함수의 매개변수
-    Pthread_create(&tid, NULL, thread, (void *)connfd);
-
-    // doit(connfd);
-
-    // Close(connfd);
+    // Pthread_create : 쓰레드 생성 함수
+    // 첫 번째 인자 : 쓰레드 식별자
+    // 두 번째 인자 : 쓰레드 특성 지정 (기본: NULL)
+    // 세 번째 인자 : 쓰레드 함수
+    // 네 번째 인자 : 쓰레드 함수의 매개변수
+    Pthread_create(&tid, NULL, thread, connfd);
   }
   return 0;
 }
 
-void *thread(void *vargsp)
-{
-  int connfd = (int)vargsp;
+// thread : 쓰레드를 통해 수행할 기능을 담은 함수
+void *thread(void *vargsp){
+  // 인자로 받은 vargsp(connfd)를 connfd에 담아준다. 
+  int connfd = *((int *)vargsp);
+  
+  // Pthread_detach : 쓰레드를 분리하는 함수
+  // 이를 통해 해당 쓰레드가 종료되면 시스템에서 자동으로 메모리를 반환시킬 수 있다.
   Pthread_detach(pthread_self());
+  Free(vargsp);
+
+  // doit 함수 호출 후, connfd를 닫는다.
   doit(connfd);
   Close(connfd);
 }
 
-void doit(int connfd)
-{
+// doit 함수는 thread 함수에서 호출되며, connfd를 인자로 받는다.
+void doit(int connfd){
   int end_serverfd;
-
   char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
   char endserver_http_header[MAXLINE];
   char hostname[MAXLINE], path[MAXLINE];
@@ -119,23 +129,23 @@ void doit(int connfd)
 
   Rio_readinitb(&rio, connfd);
   Rio_readlineb(&rio, buf, MAXLINE);
-  sscanf(buf, "%s %s %s", method, uri, version); // read the client reqeust line
+  sscanf(buf, "%s %s %s", method, uri, version); // 클라이언트 요청 라인을 읽어온다.
 
-  if (strcasecmp(method, "GET"))
-  {
+  if (strcasecmp(method, "GET")){
     printf("Proxy does not implement the method");
     return;
   }
 
+  // 캐시에서 사용할 url을 저장할 문자열 배열
   char url_store[100];
+  // uri를 복사해서 url_store에 담는다.
   strcpy(url_store, uri);
 
-  // the url is cached?
   int cache_index;
-  // in cache then return the cache content
-  if ((cache_index = cache_find(url_store)) != -1)
-  {
+  // cache_find : url을 통해 캐시 블럭의 인덱스를 확인하는 함수
+  if ((cache_index = cache_find(url_store)) != -1){
     readerPre(cache_index);
+    // 캐시의 내용을 connfd에 담아 클라이언트에게 전송한다.
     Rio_writen(connfd, cache.cacheobjs[cache_index].cache_obj, strlen(cache.cacheobjs[cache_index].cache_obj));
     readerAfter(cache_index);
     return;
@@ -149,8 +159,7 @@ void doit(int connfd)
 
   // connect to the end server
   end_serverfd = connect_endServer(hostname, port, endserver_http_header);
-  if (end_serverfd < 0)
-  {
+  if (end_serverfd < 0){
     printf("connection failed\n");
     return;
   }
@@ -164,49 +173,43 @@ void doit(int connfd)
   char cachebuf[MAX_OBJECT_SIZE];
   int sizebuf = 0;
   size_t n;
-  while ((n = Rio_readlineb(&server_rio, buf, MAXLINE)) != 0)
-  {
+  while ((n = Rio_readlineb(&server_rio, buf, MAXLINE)) != 0){
     // printf("proxy received %ld bytes, then send\n", n);
     sizebuf += n;
+    // end server로 부터 받은 메시지를 클라이언트에게 전송한다.
     if (sizebuf < MAX_OBJECT_SIZE)
       strcat(cachebuf, buf);
     Rio_writen(connfd, buf, n);
   }
   Close(end_serverfd);
 
-  // store it
-  if (sizebuf < MAX_OBJECT_SIZE)
-  {
+  // 캐시 버퍼의 내용을 새롭게 캐시 블럭에 담는다.
+  if (sizebuf < MAX_OBJECT_SIZE){
     cache_uri(url_store, cachebuf);
   }
 }
 
-void build_http_header(char *http_header, char *hostname, char *path, int port, rio_t *client_rio)
-{
+void build_http_header(char *http_header, char *hostname, char *path, int port, rio_t *client_rio){
   char buf[MAXLINE], request_hdr[MAXLINE], other_hdr[MAXLINE], host_hdr[MAXLINE];
 
   // request line
   sprintf(request_hdr, requestline_hdr_format, path);
 
   // get other request header for client rio and change it
-  while (Rio_readlineb(client_rio, buf, MAXLINE) > 0)
-  {
+  while (Rio_readlineb(client_rio, buf, MAXLINE) > 0){
     if (strcmp(buf, endof_hdr) == 0)
       break; // EOF
 
-    if (!strncasecmp(buf, host_key, strlen(host_key)))
-    {
+    if (!strncasecmp(buf, host_key, strlen(host_key))){
       strcpy(host_hdr, buf);
       continue;
     }
 
-    if (!strncasecmp(buf, connection_key, strlen(connection_key)) && !strncasecmp(buf, proxy_connection_key, strlen(proxy_connection_key)) && !strncasecmp(buf, user_agent_key, strlen(user_agent_key)))
-    {
+    if (!strncasecmp(buf, connection_key, strlen(connection_key)) && !strncasecmp(buf, proxy_connection_key, strlen(proxy_connection_key)) && !strncasecmp(buf, user_agent_key, strlen(user_agent_key))){
       strcat(other_hdr, buf);
     }
   }
-  if (strlen(host_hdr) == 0)
-  {
+  if (strlen(host_hdr) == 0){
     sprintf(host_hdr, host_hdr_format, hostname);
   }
   sprintf(http_header, "%s%s%s%s%s%s%s",
@@ -221,16 +224,14 @@ void build_http_header(char *http_header, char *hostname, char *path, int port, 
 }
 
 // Connect to the end server
-inline int connect_endServer(char *hostname, int port, char *http_header)
-{
+inline int connect_endServer(char *hostname, int port, char *http_header){
   char portStr[100];
   sprintf(portStr, "%d", port);
   return Open_clientfd(hostname, portStr);
 }
 
 // parse the uri to get hostname, file path, port
-void parse_uri(char *uri, char *hostname, char *path, int *port)
-{
+void parse_uri(char *uri, char *hostname, char *path, int *port){
   *port = 80;
   char *pos = strstr(uri, "//");
 
@@ -238,36 +239,30 @@ void parse_uri(char *uri, char *hostname, char *path, int *port)
 
   char *pos2 = strstr(pos, ":");
   // sscanf(pos, "%s", hostname);
-  if (pos2 != NULL)
-  {
+  if (pos2 != NULL){
     *pos2 = '\0';
     sscanf(pos, "%s", hostname);
     sscanf(pos2 + 1, "%d%s", port, path);
   }
-  else
-  {
+  else{
     pos2 = strstr(pos, "/");
-    if (pos2 != NULL)
-    {
+    if (pos2 != NULL){
       *pos2 = '\0'; // 중간에 끊으려고
       sscanf(pos, "%s", hostname);
       *pos2 = '/';
       sscanf(pos2, "%s", path);
     }
-    else
-    {
+    else{
       scanf(pos, "%s", hostname);
     }
   }
   return;
 }
 
-void cache_init()
-{
+void cache_init(){
   cache.cache_num = 0;
   int i;
-  for (i = 0; i < CACHE_OBJS_COUNT; i++)
-  {
+  for (i = 0; i < CACHE_OBJS_COUNT; i++){
     cache.cacheobjs[i].LRU = 0;
     cache.cacheobjs[i].isEmpty = 1;
 
@@ -276,13 +271,17 @@ void cache_init()
     // 세 번째: 초기 값
     Sem_init(&cache.cacheobjs[i].wmutex, 0, 1);
     Sem_init(&cache.cacheobjs[i].rdcntmutex, 0, 1);
-
+    // 세마포어란?
+    // 공유된 하나의 자원에 여러개의 프로세스가 동시에 접근할대 발생하는 문제를 다루는 방법. 
+    // -> 한번에 하나의 프로세스만 접근 가능하도록 만들어줘야함. 
+    // -> 이 방법이 프로세스에서 사용될때는 세마포어, 쓰레드에서는 뮤텍스 라고 함. 
+    // semaphore 값을 할당하는데 이때 값이 0이면 접근할 수 없고 0보다 크면 해당자원에 접근가능
+    // 초기값을 1 로 놓는 이유.
     cache.cacheobjs[i].readCnt = 0;
   }
 }
 
-void readerPre(int i)
-{
+void readerPre(int i){
   P(&cache.cacheobjs[i].rdcntmutex);
   cache.cacheobjs[i].readCnt++;
   if (cache.cacheobjs[i].readCnt == 1)
@@ -290,8 +289,7 @@ void readerPre(int i)
   V(&cache.cacheobjs[i].rdcntmutex);
 }
 
-void readerAfter(int i)
-{
+void readerAfter(int i){
   P(&cache.cacheobjs[i].rdcntmutex);
   cache.cacheobjs[i].readCnt--;
   if (cache.cacheobjs[i].readCnt == 0)
@@ -299,11 +297,9 @@ void readerAfter(int i)
   V(&cache.cacheobjs[i].rdcntmutex);
 }
 
-int cache_find(char *url)
-{
+int cache_find(char *url){
   int i;
-  for (i = 0; i < CACHE_OBJS_COUNT; i++)
-  {
+  for (i = 0; i < CACHE_OBJS_COUNT; i++){
     readerPre(i);
     if ((cache.cacheobjs[i].isEmpty == 0) && (strcmp(url, cache.cacheobjs[i].cache_url) == 0))
       break;
@@ -314,22 +310,22 @@ int cache_find(char *url)
   return i;
 }
 
-int cache_eviction()
-{
+int cache_eviction(){
   int min = LRU_MAGIC_NUMBER;
   int minindex = 0;
   int i;
-  for (i = 0; i < CACHE_OBJS_COUNT; i++)
-  {
+  for (i = 0; i < CACHE_OBJS_COUNT; i++){
     readerPre(i);
-    if (cache.cacheobjs[i].isEmpty == 1)
-    {
+    if (cache.cacheobjs[i].isEmpty == 1){
       minindex = i;
       readerAfter(i);
       break;
     }
-    if (cache.cacheobjs[i].LRU < min)
-    {
+    
+    // cache block 이 다 차있을때는 가장 낮은 LRU를 가진 cache block을 
+    // 덮어쓰기위해 minindex로 갱신하고 이 값을 리턴. 
+    // -> 'eviction policy'
+    if (cache.cacheobjs[i].LRU < min){
       minindex = i;
       min = cache.cacheobjs[i].LRU;
       readerAfter(i);
@@ -340,30 +336,26 @@ int cache_eviction()
   return minindex;
 }
 
-void writePre(int i)
-{
+void writePre(int i){
   P(&cache.cacheobjs[i].wmutex);
 }
 
-void writeAfter(int i)
-{
+void writeAfter(int i){
   V(&cache.cacheobjs[i].wmutex);
 }
 
 // update the LRU number except the new cache one
-void cache_LRU(int index)
-{
+// 자신을 제외한 cache block 들의 LRU를 내려주는 함수.
+void cache_LRU(int index){
   int i;
-  for (i = 0; i < index; i++)
-  {
+  for (i = 0; i < index; i++){
     writePre(i);
     if (cache.cacheobjs[i].isEmpty == 0 && i != index)
       cache.cacheobjs[i].LRU--;
     writeAfter(i);
   }
   i++;
-  for (i; i < CACHE_OBJS_COUNT; i++)
-  {
+  for (i; i < CACHE_OBJS_COUNT; i++){
     writePre(i);
     if (cache.cacheobjs[i].isEmpty == 0 && i != index)
     {
@@ -374,8 +366,7 @@ void cache_LRU(int index)
 }
 
 // cache the uri and content in cache
-void cache_uri(char *uri, char *buf)
-{
+void cache_uri(char *uri, char *buf){
   int i = cache_eviction();
 
   writePre(i);
